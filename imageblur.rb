cc2e14449589baf362eaaa59f9b657ca5f4a61a4
kathryntrom <52117883+kathryntrom@users.noreply.github.com>
2019-09-17 19:42:46 -0400

    Add files via upload

--- imageblur.rb
+++ imageblur.rb
@@ -1,8 +1,31 @@
 class Image
+
   def initialize(array)
     @matrix = array
-  end
-  def output_image
+    results = []
+
+    @matrix.each_with_index do |row, y_index|
+      row.each_with_index do |value, x_index|
+        if value == 1
+          results.push([y_index, x_index])
+        end
+      end
+    end
+
+    results.each do |coordinate|
+      y = coordinate[0]
+      x = coordinate[1]
+
+      #Top
+      @matrix[y-1][x] = 1 if y > 0
+      #Bottom
+      @matrix[y+1][x] = 1 if y < @matrix.length - 1
+      #Left
+      @matrix[y][x-1] = 1 if x > 0
+      #Right
+      @matrix[y][x+1] = 1 if x < @matrix[0].length - 1
+    end
+
     @matrix.each do |set|
       p set
     end
@@ -10,9 +33,14 @@ class Image
 end
 
 image = Image.new([
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0]
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ])
-image.output_image
